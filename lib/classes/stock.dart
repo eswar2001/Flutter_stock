ee640193e91a4956b9246e954cb81b9ab5f71ed3
eswar2001 <teswar2001@yahoo.co.in>
2020-07-21 11:17:03 +0530

    stable

--- lib/classes/stock.dart
+++ lib/classes/stock.dart
@@ -5,29 +5,36 @@ Stock stockFromJson(String str) => Stock.fromJson(json.decode(str));
 String stockToJson(Stock data) => json.encode(data.toJson());
 
 class Stock {
+  String exchange;
   String symbol;
   String nameOfCompany;
 
   Stock({
+    this.exchange,
     this.symbol,
     this.nameOfCompany,
   });
   factory Stock.fromJson(Map<String, dynamic> json) => Stock(
+        exchange: json["exchange"].toString(),
         symbol: json["SYMBOL"].toString(),
         nameOfCompany: json["NAME OF COMPANY"].toString(),
       );
   Map<String, dynamic> toJson() => {
+        "exchange": exchange,
         "SYMBOL": symbol,
         "NAME OF COMPANY": nameOfCompany,
       };
   Map<String, dynamic> toDb() {
     var map = Map<String, dynamic>();
+    map["exchange"] = exchange;
     map["symbol"] = symbol;
     map["name"] = nameOfCompany;
 
     return map;
   }
+
   factory Stock.fromDb(Map<String, dynamic> map) => Stock(
+        exchange: map["exchange"],
         symbol: map["symbol"],
         nameOfCompany: map["name"],
       );
